f07d7802cb8e5b1590308157b6bed28230005a0b
limeunseop <dmstjq12@naver.com>
2023-10-16 08:23:49 +0900

    Add Main Page Logo

--- src/domain/main/index.tsx
+++ src/domain/main/index.tsx
@@ -7,10 +7,13 @@ const Main = async () => {
   const appList = await getAppList()
 
   return (
-    <div className="flex gap-8">
-      {appList.map((app) => (
-        <AppIcon key={app.id} app={app} />
-      ))}
+    <div className="flex flex-col gap-8">
+      <Image src="/logo-full.png" width="300" height="240" alt="" />
+      <div className="flex justify-center gap-8">
+        {appList.map((app) => (
+          <AppIcon key={app.id} app={app} />
+        ))}
+      </div>
     </div>
   )
 }
