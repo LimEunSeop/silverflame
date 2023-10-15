c29841d5400d31b4ee5c31168682965791ef4218
limeunseop <dmstjq12@naver.com>
2023-10-15 20:39:06 +0900

    Add theme feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import './globals.css'
-import type { Metadata } from 'next'
 import localFont from 'next/font/local'
+import Script from 'next/script'
+import { ChangeEventHandler, useEffect } from 'react'
 
 const pretendard = localFont({
   //! 모듈 위치 바뀜 주의. import문의 규칙을 따를수는 없는걸까?
@@ -9,15 +12,37 @@ const pretendard = localFont({
   display: 'swap',
 })
 
-export const metadata: Metadata = {
-  title: 'SilverFlame',
-  description: "Silverflame's personal website for web applications",
-}
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const toggleTheme: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const checked = e.target.checked
+
+    if (checked) {
+      document.documentElement.dataset.theme = 'light'
+    } else {
+      document.documentElement.dataset.theme = 'dark'
+    }
+  }
+
   return (
     <html lang="ko" className={`${pretendard.variable} font-sans`}>
-      <body>{children}</body>
+      <body>
+        <input type="checkbox" className="toggle toggle-warning absolute top-2 right-2" onChange={toggleTheme} />
+        {children}
+        {/* appDir 에서는 app/layout.tsx 에 넣어도 된다고 함. See: https://nextjs.org/docs/app/api-reference/components/script#beforeinteractive */}
+        <Script
+          id="script-init-theme"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          if (
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+          ) {
+            document.documentElement.dataset.theme = 'dark'
+          }
+        `,
+          }}
+        />
+      </body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,5 +14,10 @@ const config: Config = {
     },
   },
   plugins: [require('daisyui')],
+  // Daisy UI dark모드 조건에 대응하기 위함: See: https://daisyui.com/docs/themes/#-2
+  daisyui: {
+    themes: false,
+  },
+  darkMode: ['class', '[data-theme="dark"]'],
 }
 export default config
