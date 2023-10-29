ce829ab9fc98b2dba131d2a9a78907d6fc436d66
limeunseop <dmstjq12@naver.com>
2023-10-30 02:32:10 +0900

    Fix responsive size of Project

--- src/domain/careers/experiences/portfolio/Project.tsx
+++ src/domain/careers/experiences/portfolio/Project.tsx
@@ -18,14 +18,14 @@ const Project = ({ project }: Props) => {
     <div className="hero bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
         <div
-          className={`carousel carousel-center rounded-box max-w-lg space-x-4 bg-neutral p-4 ${
+          className={`carousel carousel-center rounded-box max-w-xs space-x-4 bg-neutral p-4 sm:max-w-lg ${
             mobileImages.length === 0 ? 'inline-flex' : desktopImages.length === 0 ? 'hidden' : 'hidden md:inline-flex'
           }`}
         >
           {desktopImages.map((image) => (
             <div key={image.id} className={`carousel-item`}>
               <div className="mockup-window border bg-base-300">
-                <div className="relative flex aspect-[16/10] w-80 justify-center bg-base-200 px-4 py-16">
+                <div className="relative flex aspect-[16/10] w-60 justify-center bg-base-200 px-4 py-16 sm:w-80">
                   <Image
                     src={image.url}
                     fill
@@ -39,9 +39,8 @@ const Project = ({ project }: Props) => {
           ))}
         </div>
 
-        {/* <div className="w-[200px]"> */}
         <div
-          className={`carousel carousel-center rounded-box max-w-lg bg-neutral p-4 ${
+          className={`carousel carousel-center rounded-box max-w-xs bg-neutral p-4 sm:max-w-lg ${
             desktopImages.length === 0
               ? 'inline-flex md:inline-flex'
               : mobileImages.length === 0
@@ -68,7 +67,6 @@ const Project = ({ project }: Props) => {
             </div>
           ))}
         </div>
-        {/* </div> */}
 
         <div>
           <h1 className="text-5xl font-bold">{project.name}</h1>
