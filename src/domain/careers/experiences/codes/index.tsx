9d4931b2d3db0140c553bf4a65e973561f3d11b8
limeunseop <dmstjq12@naver.com>
2023-10-31 18:22:29 +0900

    Add prism react 구현 예시

--- src/domain/careers/experiences/codes/index.tsx
+++ src/domain/careers/experiences/codes/index.tsx
@@ -1,4 +1,36 @@
+'use client'
+import { Highlight, themes } from 'prism-react-renderer'
+
+const codeBlock = `
+const GroceryItem: React.FC<GroceryItemProps> = ({ item }) => {
+  return (
+    <div>
+      <h2>{item.name}</h2>
+      <p>Price: {item.price}</p>
+      <p>Quantity: {item.quantity}</p>
+    </div>
+  );
+}
+`
+
 const CareersCodes = () => {
-  return <div>CareersCodes</div>
+  return (
+    <div>
+      <Highlight theme={themes.shadesOfPurple} code={codeBlock} language="tsx">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <div style={style} className="mockup-code">
+            {tokens.map((line, i) => (
+              <pre key={i} {...getLineProps({ line })} data-prefix={i + 1}>
+                {/* <span>{i + 1}</span> */}
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token })} />
+                ))}
+              </pre>
+            ))}
+          </div>
+        )}
+      </Highlight>
+    </div>
+  )
 }
 export default CareersCodes
