0a1790fc5a79c8c443c9b239af14bad04908f42c
limeunseop <dmstjq12@naver.com>
2023-11-04 00:52:07 +0900

    Fix Contact page

--- src/domain/careers/contact/index.tsx
+++ src/domain/careers/contact/index.tsx
@@ -1,3 +1,5 @@
+import GithubLink from '@/components/links/GithubLink'
+
 const CareersContact = () => {
   return (
     <div className="hero flex-1">
@@ -8,9 +10,11 @@ const CareersContact = () => {
             꾸준히 정진하는 개발자가 되겠습니다.
             <br />
             무엇이든 연락해주시면 성심성의껏 답변드리겠습니다.
+            <br />
+            <GithubLink href="https://github.com/limeunseop" />
           </p>
           <a href="mailto:dmstjq92@gmail.com" className="btn btn-primary">
-            연락하기
+            Email
           </a>
         </div>
       </div>
