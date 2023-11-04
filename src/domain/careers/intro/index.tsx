61080976a7dbdf17c7b62d789cc405e77c981526
limeunseop <dmstjq12@naver.com>
2023-11-04 17:42:17 +0900

    Update text

--- src/domain/careers/intro/index.tsx
+++ src/domain/careers/intro/index.tsx
@@ -16,7 +16,7 @@ const CareersIntro = () => {
           </h1>
           <p className="py-4 md:py-6">
             꾸준히 고민하고 성장하고 있는 프론트엔드 개발자입니다. 여러 공식 문서를 읽어가며 다져놓은 제 지식을 기반으로
-            조직의 문제해결에 보탬이 되고 싶습니다. 점점 더 굳건해져 가고 있는 제 경험들을 둘러봐주세요.
+            문제해결에 보탬이 되고 싶습니다. 점점 더 굳건해져 가고 있는 제 경험들을 둘러봐주세요.
           </p>
           <Link href="/careers/experiences" className="btn btn-primary">
             Explore
