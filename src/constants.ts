a40504f27905a1477fa9e173ce280332c260c127
limeunseop <dmstjq12@naver.com>
2023-11-05 21:41:34 +0900

    Fix Notion Site Link

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const THEME_DARK = 'silverflame-dark'
 
 export const LINK_GITHUB = 'https://github.com/limeunseop'
 export const LINK_BLOG = 'https://velog.io/@seop'
-export const LINK_NOTION = 'https://limeunseop.notion.site/limeunseop/b56b7e9c10ef4616a76693e33b0bd1ee'
+export const LINK_NOTION = 'https://limeunseop.notion.site/2827a91dd8b5499d89b09033cf948a3c'
 export const LINK_EMAIL = 'mailto:dmstjq92@gmail.com'
