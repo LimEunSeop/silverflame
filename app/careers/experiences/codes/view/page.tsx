d7ec1499872b1cea9c035f3642a2036c39bec420
limeunseop <dmstjq12@naver.com>
2023-11-06 10:55:52 +0900

    당장 불필요한 설정 제거 (설명있음)
    - https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#fetchcache
    - 위 문서에 따르면, 정말 필요할 때 써야할 고급 옵션이라고 쓰여져 있다.

--- app/careers/experiences/codes/view/page.tsx
+++ app/careers/experiences/codes/view/page.tsx
@@ -3,7 +3,6 @@ import CareersCodesView from '@/domain/careers/experiences/codes/view'
 //TODO: 앞으로의 데이터 양이 많아질 페이지라 페이지를 static 렌더링 하는데 한계가 있을것이다.
 //TODO: 렌더링 최적화를 위해 loading.js 와 같은 Suspense 바운더리를 주고 나중에 로딩 UI를 꾸며보도록 하자.
 export const dynamic = 'force-dynamic'
-export const fetchCache = 'force-no-store'
 
 const CareersCodesViewPage = ({
   searchParams,
