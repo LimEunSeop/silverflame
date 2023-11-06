1b81fcae75b2ed80729c00b3fb14693d8cbb3bed
limeunseop <dmstjq12@naver.com>
2023-11-06 10:50:55 +0900

    Fix refresh 304 empty page
    - https://github.com/vercel/next.js/issues/54782 에 따르면, 싱글턴 모듈은 요청 시 유실될 수 있으므로, 매번 생성될 수 있도록 하는것이 좋다고는 하다.
    - 또 다른 관련문서: https://github.com/vercel/next.js/issues/55660 https://github.com/vercel/next.js/issues/56018

--- src/domain/careers/resume/index.tsx
+++ src/domain/careers/resume/index.tsx
@@ -8,18 +8,18 @@ import { fromHtml } from 'hast-util-from-html'
 
 const MARKDOWN_RESUME_URL = 'https://raw.githubusercontent.com/LimEunSeop/my-resume/main/README.md'
 
-//TODO: react element를 content에 넣는 좋은 방법 없을까?
-// LinkIcon
-const iconHast = fromHtml(
-  `
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-4 h-4">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-</svg>
-`,
-  { fragment: true },
-)
-
 const CareersResume = async () => {
+  //TODO: react element를 content에 넣는 좋은 방법 없을까?
+  // LinkIcon
+  const iconHast = fromHtml(
+    `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-4 h-4">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+    </svg>
+    `,
+    { fragment: true },
+  )
+
   const markdownContent = await fetch(MARKDOWN_RESUME_URL, {
     next: {
       revalidate: 60,
