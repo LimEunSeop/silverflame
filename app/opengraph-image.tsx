b195b165f98fd8133c46be03b7e038f34ec7da00
limeunseop <dmstjq12@naver.com>
2023-11-23 13:00:31 +0900

    Revert "fix(opengraph): adjust image size to kakao preview"
    This reverts commit ebc4d1b1fc7bc0daa519b4311c6de0da14d7e158.

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -6,8 +6,8 @@ export const runtime = 'edge'
 // Image metadata
 export const alt = 'Silverflame Full Logo'
 export const size = {
-  width: 800,
-  height: 400,
+  width: 1200,
+  height: 630,
 }
 
 export const contentType = 'image/png'
